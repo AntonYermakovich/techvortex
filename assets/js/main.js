006aea85d18a7b6b087459395c2b2b80e53f1829
Anton Yermakovich <antonyermakovich@gmail.com>
2024-09-08 00:59:36 +0300

    finished project

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", function () {
     rewind: true,
     pagination: false,
     arrows: false,
-    padding: '30px',
+    padding: "30px",
 
     breakpoints: {
       1400: {
@@ -23,20 +23,93 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 // Burger
-const menuIcon = document.querySelector('.header__icon');
-const headerLogo = document.querySelector('.header__logo');
-const headerMenu = document.querySelector('.menu');
+const menuIcon = document.querySelector(".header__icon");
+const headerLogo = document.querySelector(".header__logo");
+const headerMenu = document.querySelector(".menu");
 
-menuIcon.addEventListener('click', menuOpenHandler)
+menuIcon.addEventListener("click", menuOpenHandler);
 
-function menuOpenHandler () {
-  if(this.classList.contains('header__icon_active')) {
-    this.classList.remove('header__icon_active')
-    headerLogo.classList.remove('header__logo_light')
-    headerMenu.classList.remove('menu_show')
+function menuOpenHandler() {
+  if (this.classList.contains("header__icon_active")) {
+    this.classList.remove("header__icon_active");
+    headerLogo.classList.remove("header__logo_light");
+    headerMenu.classList.remove("menu_show");
   } else {
-    this.classList.add('header__icon_active')
-    headerLogo.classList.add('header__logo_light')
-    headerMenu.classList.add('menu_show')
+    this.classList.add("header__icon_active");
+    headerLogo.classList.add("header__logo_light");
+    headerMenu.classList.add("menu_show");
   }
 }
+
+// mask for number
+const phone = document.getElementById("phone");
+const maskOptions = {
+  mask: "+{7}(000) 000-00-00",
+  lazy: true,
+};
+const mask = IMask(phone, maskOptions);
+
+// validate form
+const validator = new JustValidate("#form");
+validator
+  .addField("#name", [
+    {
+      rule: "required",
+    },
+    {
+      rule: "minLength",
+      value: 3,
+    },
+    {
+      rule: "maxLength",
+      value: 15,
+    },
+  ])
+  .addField("#phone", [
+    {
+      rule: "required",
+    },
+  ])
+  .addField("#email", [
+    {
+      rule: "required",
+    },
+    {
+      rule: "email",
+    },
+  ]);
+
+document.querySelector("#form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  if (validator.isValid && mask.masked.isComplete) {
+    const formData = {
+        name:e.target.name.value,
+        phone:e.target.phone.value,
+        email:e.target.email.value 
+    }
+
+    await fetch("mail.php", {
+      method: "POST",
+      body: JSON.stringify(formData),
+    });
+ 
+    Swal.fire({
+      title: "Ваше сообщение отправлено!",
+      text: "Наши специалисты скоро с вами свяжутся!",
+      icon: "success",
+    });
+    e.target.reset();
+  } else {
+    Swal.fire({
+      title: "Ваше сообщение не отправлено!",
+      text: "Пожалуйста заполните все поля правильно!",
+      icon: "error",
+    });
+  }
+});
+
+AOS.init({
+    offset: 120,
+    once: true
+});
---
+++ mail.php
@@ -0,0 +1,55 @@
+<?php
+// Подключение библиотеки
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php';
+require 'phpmailer/Exception.php';
+
+// Получение данных
+$json = file_get_contents('php://input'); // Получение json строки
+$data = json_decode($json, true); // Преобразование json
+
+$name = $data['name'];
+$phone = $data['phone'];
+$email = $data['email'];
+
+$title = 'Заявка с сайта TechVortex'; // Название письма
+$body = '<p>Имя: <strong>'.$name.'</strong></p>';
+$body .= '<p>Телефон: <strong>'.$phone.'</strong></p>';
+$body .= '<p>E-mail: <strong>'.$email.'</strong></p>';
+
+// Настройки PHPMailer
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+
+try {
+  $mail->isSMTP();
+  $mail->CharSet = 'UTF-8';
+  $mail->SMTPAuth   = true;
+
+  // Настройки почты отправителя
+  $mail->Host       = 'smtp.gmail.com'; // SMTP сервера вашей почты
+  $mail->Username   = 'tonymind58@gmail.com'; // Логин на почте
+  $mail->Password   = 'siva xets txyk hhll'; // Пароль на почте
+  $mail->SMTPSecure = 'ssl';
+  $mail->Port       = 465;
+
+  $mail->setFrom('tonymind58@gmail.com'); // Адрес самой почты и имя отправителя
+
+  // Получатель письма
+  $mail->addAddress('rycoka5@mail.ru');
+  $mail->addAddress('anton.yermakovich@yandex.by');
+  $mail->addAddress('tonymind58@gmail.com');
+
+  // Отправка сообщения
+  $mail->isHTML(true);
+  $mail->Subject = $title;
+  $mail->Body = $body;
+
+  $mail->send('d');
+
+  // Сообщение об успешной отправке
+  echo ('Сообщение успешно отправлено.');
+
+} catch (Exception $e) {
+  header('HTTP/1.1 400 Bad Request');
+  echo('Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}');
+}
