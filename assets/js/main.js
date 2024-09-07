9216a1e7cfebb7eafe2a73621e6bd5b338609953
Anton Yermakovich <antonyermakovich@gmail.com>
2024-09-07 20:38:56 +0300

    add adaptive

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,10 +6,37 @@ document.addEventListener("DOMContentLoaded", function () {
     rewind: true,
     pagination: false,
     arrows: false,
+    padding: '30px',
+
     breakpoints: {
       1400: {
         perPage: 3,
       },
+      1200: {
+        perPage: 2,
+      },
+      768: {
+        perPage: 1,
+      },
     },
   }).mount();
 });
+
+// Burger
+const menuIcon = document.querySelector('.header__icon');
+const headerLogo = document.querySelector('.header__logo');
+const headerMenu = document.querySelector('.menu');
+
+menuIcon.addEventListener('click', menuOpenHandler)
+
+function menuOpenHandler () {
+  if(this.classList.contains('header__icon_active')) {
+    this.classList.remove('header__icon_active')
+    headerLogo.classList.remove('header__logo_light')
+    headerMenu.classList.remove('menu_show')
+  } else {
+    this.classList.add('header__icon_active')
+    headerLogo.classList.add('header__logo_light')
+    headerMenu.classList.add('menu_show')
+  }
+}
