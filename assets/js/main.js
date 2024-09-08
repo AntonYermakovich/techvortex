15955e7d573ebebbf71ac0c22dacd959598c28dd
Anton Yermakovich <antonyermakovich@gmail.com>
2024-09-08 17:32:41 +0300

    finished project

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,16 +26,16 @@ document.addEventListener("DOMContentLoaded", function () {
 const menuIcon = document.querySelector(".header__icon");
 const headerLogo = document.querySelector(".header__logo");
 const headerMenu = document.querySelector(".menu");
-const menuLinks = document.querySelectorAll('.menu__link')
+const menuLinks = document.querySelectorAll(".menu__link");
 
 menuIcon.addEventListener("click", menuOpenHandler);
-menuLinks.forEach(link => link.addEventListener('click', scrollToSection))
+menuLinks.forEach((link) => link.addEventListener("click", scrollToSection));
 
 function scrollToSection() {
   menuIcon.classList.remove("header__icon_active");
   headerLogo.classList.remove("header__logo_light");
   headerMenu.classList.remove("menu_show");
-  document.body.classList.remove('hidden')
+  document.body.classList.remove("hidden");
 }
 
 function menuOpenHandler() {
@@ -43,22 +43,24 @@ function menuOpenHandler() {
     this.classList.remove("header__icon_active");
     headerLogo.classList.remove("header__logo_light");
     headerMenu.classList.remove("menu_show");
-    document.body.classList.remove('hidden')
+    document.body.classList.remove("hidden");
   } else {
     this.classList.add("header__icon_active");
     headerLogo.classList.add("header__logo_light");
     headerMenu.classList.add("menu_show");
-    document.body.classList.add('hidden')
+    document.body.classList.add("hidden");
   }
 }
 
 // mask for number
 const phone = document.getElementById("phone");
+// const maskOptions = {
+//   mask: "+{7}(000) 000-00-00",
+//   lazy: true,
+// };
 const maskOptions = {
   mask: "+{7}(000) 000-00-00",
-  // lazy: true,
-  lazy: false,
-  overwrite: 'shift',
+  lazy: true,
 };
 const mask = IMask(phone, maskOptions);
 
@@ -68,28 +70,28 @@ validator
   .addField("#name", [
     {
       rule: "required",
-      errorMessage: "Имя обязательно для заполнения"
+      errorMessage: "Имя обязательно для заполнения",
     },
     {
       rule: "minLength",
       value: 2,
-      errorMessage: "Минимальная длина имени 2 символа"
-    }
+      errorMessage: "Минимальная длина имени 2 символа",
+    },
   ])
   .addField("#phone", [
     {
       rule: "required",
-      errorMessage: "Введите ваш номер телефона"
+      errorMessage: "Введите ваш номер телефона",
     },
   ])
   .addField("#email", [
     {
       rule: "required",
-      errorMessage: "Введите ваш e-mail"
+      errorMessage: "Введите ваш e-mail",
     },
     {
       rule: "email",
-      errorMessage: "Это не e-mail"
+      errorMessage: "Это не e-mail",
     },
   ]);
 
@@ -98,16 +100,16 @@ document.querySelector("#form").addEventListener("submit", async (e) => {
 
   if (validator.isValid && mask.masked.isComplete) {
     const formData = {
-        name:e.target.name.value,
-        phone:e.target.phone.value,
-        email:e.target.email.value 
-    }
+      name: e.target.name.value,
+      phone: e.target.phone.value,
+      email: e.target.email.value,
+    };
 
     await fetch("mail.php", {
       method: "POST",
       body: JSON.stringify(formData),
     });
- 
+
     Swal.fire({
       title: "Ваше сообщение отправлено!",
       text: "Наши специалисты скоро с вами свяжутся!",
@@ -118,6 +120,6 @@ document.querySelector("#form").addEventListener("submit", async (e) => {
 });
 
 AOS.init({
-    offset: 120,
-    once: true
+  offset: 120,
+  once: true,
 });
